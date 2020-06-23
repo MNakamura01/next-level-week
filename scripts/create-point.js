287e6e39e4f62707967c7fd73eb91c12eec3610a
MNakamura01 <mrnnakamura01@gmail.com>
2020-06-23 15:23:13 -0300

    atualizações

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -45,3 +45,41 @@ document
 	.querySelector("select[name=uf]") //QUERYSELECTOR = procura no documetno html o elemento entre parênteses.
 	.addEventListener("change", getCities ) // EVENTLISTENER = procura por eventos (onclick, change...)
 	// () => {} = arrow function = fução anonima
+
+
+
+//ITENS DE COLETA
+
+const itemsToCollect = document.querySelectorAll('.items-grid li')
+
+for (const item of itemsToCollect) {
+	document.addEventListener("click", handSelectedItem ) // EVENTLISTENER = procura por eventos (onclick, change...)
+
+}
+
+let selectedItems = []
+
+function handSelectedItem(event) {
+	// adicionar ou remover uma classe com js 
+	const itemLi = event.target
+
+	itemLi.classList.toggle("selected") //
+	//.add= adiciona
+	//.remove
+	//.toogle = adicionar ou remover se ja existir a classe
+	const itemId = itemLi.dataset.id
+
+
+
+	//verificar se existem itens selecionados. 
+	//Se sim, pegar os itens selecionados
+	const alreadySelected = selectedItems.findIndex(item => {
+		const itemFound = item === itemId // retorna true or false
+		return itemFound
+	})
+
+	//se já estiver selecionado, tirar da seleção
+	//se não estiver selecionado, adicionar à seleção
+
+	//atualizar o campo escondido com os itens selecionados
+}
